8955d6f6186b238664676c7618d6273a8e3a8b69
Mallory <mallorybrickerd@gmail.com>
2025-04-13 18:04:04 +0200

    feature/clean up (#2)
    * fix: cache pre-commit hooks
    
    * fix: rename management resources
    
    * fix: CI step names

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "azurerm" {
     subscription_id      = "7bc4b637-e7b0-4cf5-8105-b46e1c86cb83"
-    resource_group_name  = "rg-projectfactory-mgmt"
-    storage_account_name = "saprojectfactorymgmt"
-    container_name       = "mgmt-state"
+    resource_group_name  = "rg-factory-mgmt"
+    storage_account_name = "safactorymgmt"
+    container_name       = "factory-mgmt-state"
     key                  = "terraform.tfstate"
   }
 }
