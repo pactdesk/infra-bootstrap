5ebe779378a073f72cdca6f94ae2f12b7cfaffae
Mallory <mallorybrickerd@gmail.com>
2025-04-13 19:31:39 +0200

    fix: remove backend storage container (#4)

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     subscription_id      = "7bc4b637-e7b0-4cf5-8105-b46e1c86cb83"
     resource_group_name  = "rg-factory-mgmt"
     storage_account_name = "safactorymgmt"
-    container_name       = "factory-mgmt-state"
+    container_name       = "bootstrap-mgmt-state"
     key                  = "terraform.tfstate"
   }
 }
